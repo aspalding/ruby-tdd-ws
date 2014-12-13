658658608cfe84df5b4294d78520749b443f6f17
aspalding <andrewtspalding@gmail.com>
2014-12-13 11:36:54 -0600

    add more tests

--- bowling_spec.rb
+++ bowling_spec.rb
@@ -13,8 +13,30 @@ describe Bowling, "#score" do
     game.score.should eq(20)
   end
   
+  xit "should return 16 spare and one three" do
+    g.hits(5)
+    g.hits(5)
+    g.hits(3)
+    
+    many(17, 0)
+    game.score.should eq(16)
+  end
+  
+  xit "should return 24 strike and one three and one four" do
+    game.hits(10)
+    game.hits(3)
+    game.hits(4)
+    
+    many(17, 0)
+    game.score.should eq(24)
+  end
+  
+  xit "300 perfect" do
+    many(12, 10)
+    game.score.should eq(300)
+  end
+  
   def many(n, p)
     n.times { game.hits(p) }
   end
-
 end
